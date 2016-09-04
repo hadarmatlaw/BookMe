5a0e6da50764657034d6b57d91cec20c1d16470c
Ofri Shani <ofrishani10@gmail.com>
2016-09-04 21:56:22 +0300

    ofri try

--- app/src/main/java/b/hadar/bookme/MainActivity.java
+++ app/src/main/java/b/hadar/bookme/MainActivity.java
@@ -24,7 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 public class MainActivity extends Activity {
 
-
+//ofri change
     DatabaseHelper mydb;
     EditText bookName, Author;
     Button addDatabtn;
